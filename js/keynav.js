57d39c36ca5e87cf298a39523a5024cca112ebfe
Mark Conroy <mark@mark.ie>
2024-07-23 13:23:51 +0100

    forces clicking enter for keynav to take effect

--- js/keynav.js
+++ js/keynav.js
@@ -5,7 +5,6 @@
 (function keyNavScript(Drupal) {
   Drupal.behaviors.keyNav = {
     attach: function (context) {
-      let redirectTimeout;
       let keySequence = ''; // Initialize variable to store key presses
       const sequences = fetch(`${window.location.origin}/modules/contrib/localgov_keynav/js/keynav-sequences.json`).then((response) => {
         return response.json();
@@ -15,13 +14,9 @@
       const ckeditors = document.querySelectorAll('.form-textarea-wrapper');
       const toolbarLinks = document.querySelectorAll('.toolbar-menu-administration > .toolbar-menu > li > a');
 
-      function handleTimeout(link) {
-        clearTimeout(redirectTimeout);
-        redirectTimeout = setTimeout(() => {
+      function handleToolbarItems(link) {
           window.location.href = link.href;
-          console.log(link.href);
           keySequence = '';
-        }, 750);
       }
 
       // Define a named event listener function for keyup events.
@@ -29,72 +24,90 @@
         // Add the pressed key to the sequence.
         keySequence += event.key.toLowerCase();
 
-        // Check if the sequence starts with 'lgd'
-        if (keySequence.startsWith('lgd')) {
+        // If the esc key is pressed, reset the key sequence.
+        if (event.key === "Escape") {
+          keySequence = '';
+        }
 
-          // For each sequence in the JSON file, check if the current sequence matches
-          sequences.then((data) => {
-            data.forEach((sequence) => {
-              if (keySequence === 'lgd' + sequence.keynavSequence) {
-                // Clear any existing timeout to reset the delay if another key is pressed
-                clearTimeout(redirectTimeout);
-                // Set a new timeout to redirect after a half-second delay
-                redirectTimeout = setTimeout(() => {
-                  window.location.href = sequence.url;
-                  keySequence = ''; // Reset the key sequence after redirecting
-                }, 1000);
+        // Don't do anything until the enter key is pressed.
+        if (event.key === "Enter") {
+          // We don't want 'enter' at the end of the sequence, or else we'll have
+          // a sequence like lgdasenter which won't match anything.
+          keySequence = keySequence.replace('enter', '');
+
+          // Check if the sequence starts with 'lgd'
+          if (keySequence.startsWith('lgd')) {
+
+            // For each sequence in the JSON file, check if the current sequence matches
+            sequences.then((data) => {
+              data.forEach((sequence) => {
+                if (keySequence === 'lgd' + sequence.keynavSequence) {
+                    window.location.href = sequence.url;
+                    keySequence = '';
+                }
+              });
+            });
+
+            // Foreach item in toolbarlinks, check if the current sequence matches
+            // that items index
+            // This is for top level toolbar items.
+            toolbarLinks.forEach((topLevelItem, topLevelItemIndex) => {
+               if (keySequence === `lgdt${(topLevelItemIndex + 1)}`) {
+                handleToolbarItems(topLevelItem);
+              }
+              // This is for second level toolbar items.
+              // For exmaple, admin > structure > content types
+              // It follows the format lgdt2.3 (3rd submenu item of the second toolbar item)
+              const hasLevel1Items = topLevelItem.closest('li').querySelector('.toolbar-menu');
+              if (hasLevel1Items) {
+                const toolbarLinksLevel1ItemsLi = Array.from(hasLevel1Items.children);
+                const toolbarLinksLevel1Items = [];
+                toolbarLinksLevel1ItemsLi.forEach((li) => {
+                  toolbarLinksLevel1Items.push(li.querySelector('a'));
+                });
+                toolbarLinksLevel1Items.forEach((levelOneItem, levelOneItemIndex) => {
+                   if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}`) {
+                    handleToolbarItems(levelOneItem);
+                  }
+                  // This is for third level toolbar items.
+                  // For example, admin > structure > content types > add content type
+                  // It follows the format lgdt2.3.1 (1st submenu item of the 3rd submenu item of the second toolbar item)
+                  const hasLevel2Items = levelOneItem.closest('li').querySelector('.toolbar-menu');
+                  if (hasLevel2Items) {
+                    const toolbarLinksLevel2ItemsLi = Array.from(hasLevel2Items.children);
+                    const toolbarLinksLevel2Items = [];
+                    toolbarLinksLevel2ItemsLi.forEach((li) => {
+                      toolbarLinksLevel2Items.push(li.querySelector('a'));
+                    });
+                    toolbarLinksLevel2Items.forEach((levelTwoItem, levelTwoItemIndex) => {
+                       if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}.${(levelTwoItemIndex)}`) {
+                        handleToolbarItems(levelTwoItem);
+                      }
+                      // This is for fourth level toolbar items.
+                      // For example, admin > structure > content types > news > manage fields
+                      // It follows the format lgdt2.3.1.1 (1st submenu item of the 4th submenu item of the 3rd submenu item of the second toolbar item)
+                      const hasLevel3Items = levelTwoItem.closest('li').querySelector('.toolbar-menu');
+                      if (hasLevel3Items) {
+                        const toolbarLinksLevel3ItemsLi = Array.from(hasLevel3Items.children);
+                        const toolbarLinksLevel3Items = [];
+                        toolbarLinksLevel3ItemsLi.forEach((li) => {
+                          toolbarLinksLevel3Items.push(li.querySelector('a'));
+                        });
+                        toolbarLinksLevel3Items.forEach((levelThreeItem, levelThreeItemIndex) => {
+                           if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}.${(levelTwoItemIndex)}.${(levelThreeItemIndex)}`) {
+                            handleToolbarItems(levelThreeItem);
+                          }
+                        });
+                      }
+                    });
+                  }
+                });
               }
             });
-          });
-          // Foreach item in toolbarlinks, check if the current sequence matches
-          // that items index
-          toolbarLinks.forEach((topLevelItem, topLevelItemIndex) => {
-            if (keySequence === `lgdt${(topLevelItemIndex + 1)}`) {
-              handleTimeout(topLevelItem);
-            }
-            const hasLevel1Items = topLevelItem.closest('li').querySelector('.toolbar-menu');
-            if (hasLevel1Items) {
-              const toolbarLinksLevel1ItemsLi = Array.from(hasLevel1Items.children);
-              const toolbarLinksLevel1Items = [];
-              toolbarLinksLevel1ItemsLi.forEach((li) => {
-                toolbarLinksLevel1Items.push(li.querySelector('a'));
-              });
-              toolbarLinksLevel1Items.forEach((levelOneItem, levelOneItemIndex) => {
-                if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}`) {
-                  handleTimeout(levelOneItem);
-                }
-                const hasLevel2Items = levelOneItem.closest('li').querySelector('.toolbar-menu');
-                if (hasLevel2Items) {
-                  const toolbarLinksLevel2ItemsLi = Array.from(hasLevel2Items.children);
-                  const toolbarLinksLevel2Items = [];
-                  toolbarLinksLevel2ItemsLi.forEach((li) => {
-                    toolbarLinksLevel2Items.push(li.querySelector('a'));
-                  });
-                  toolbarLinksLevel2Items.forEach((levelTwoItem, levelTwoItemIndex) => {
-                    if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}.${(levelTwoItemIndex)}`) {
-                      handleTimeout(levelTwoItem);
-                    }
-                    const hasLevel3Items = levelTwoItem.closest('li').querySelector('.toolbar-menu');
-                    if (hasLevel3Items) {
-                      const toolbarLinksLevel3ItemsLi = Array.from(hasLevel3Items.children);
-                      const toolbarLinksLevel3Items = [];
-                      toolbarLinksLevel3ItemsLi.forEach((li) => {
-                        toolbarLinksLevel3Items.push(li.querySelector('a'));
-                      });
-                      toolbarLinksLevel3Items.forEach((levelThreeItem, levelThreeItemIndex) => {
-                        if (keySequence === `lgdt${(topLevelItemIndex + 1)}.${(levelOneItemIndex)}.${(levelTwoItemIndex)}.${(levelThreeItemIndex)}`) {
-                          handleTimeout(levelThreeItem);
-                        }
-                      });
-                    }
-                  });
-                }
-              });
-            }
-          });
-        } else if (!'lgd'.startsWith(keySequence)) {
-          // If the current sequence cannot possibly match 'lgd', reset it
-          keySequence = '';
+          } else if (!'lgd'.startsWith(keySequence)) {
+            // If the current sequence cannot possibly match 'lgd', reset it
+            keySequence = '';
+          }
         }
 
       }
